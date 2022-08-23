b70fd933e19ab2754f71a712247d2ee101b374f1
Chandu Sai Naladala <chandu.naladala@valuemomentum.com>
2022-08-23 16:20:04 +0530

    committing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,70 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { EventsListResolver } from './events/events-list-resolver.service';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { SessionListComponent } from './events/event-details/session-list.component';
+import { DurationPipe } from './events/shared/duration.pipe';
+import { AuthService } from './user/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { EventsResolver } from './events/event-resolver.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    Error404Component,
+    SessionListComponent,
+    DurationPipe
+   
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+
+    CommonModule,
+    FormsModule,
+   BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  
+  providers: [
+    EventService,
+    EventRouteActivator,
+    AuthService,
+  
+    {
+        provide: 'canDeactivateCreateEvent',
+        useValue: checkDirtyState
+
+    },EventsListResolver,
+    EventsResolver
+  
+  ],
+
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
+
+
+
+function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
---
+++ src/app/test.spec.ts
@@ -0,0 +1,13 @@
+describe('first test',()=>{
+    it('should be true',()=>{
+        //Arrange
+        //Act
+    expect(true).toBe(true);//Assert
+})
+it('should be false',()=>{
+    expect(false).toBe(false);
+})
+it('should be true',()=>{
+    expect(true).toBe(false);
+})
+})
